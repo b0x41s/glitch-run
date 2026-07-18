ee005afe9ee49c8c16cb9b613ca1263148c0ddc2
Bas <b0x41s@hotmail.com>
2026-07-18 16:55:24 +0200

    Load Bomber Tribe AI upgrade

--- public/bomber-tribe/game.js
+++ public/bomber-tribe/game.js
@@ -1,4 +1,4 @@
 'use strict';
-['core.js', 'gameplay.js', 'render.js', 'app.js'].forEach(src => {
+['core.js', 'gameplay.js', 'ai-upgrade.js', 'render.js', 'app.js'].forEach(src => {
   document.write(`<script src="${src}"><\/script>`);
 });
